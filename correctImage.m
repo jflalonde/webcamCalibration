147e9628dc27e06c3dfb47f0b753a3936096bc5b
Jean-Francois Lalonde <jflalonde@gmail.com>
2013-10-31 22:20:23 -0400

    Reformat of comments, added support for empty inverse response function (linear).

--- correctImage.m
+++ correctImage.m
@@ -1,38 +1,33 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function corrImg = correctImage(img, camInvResponse)
-%   Corrects the image according to the inverse response function
-%  
-% 
-% Input parameters:
-%  - img: input image
-%  - camInvResponse: inverse response function (Nx3)
-%
-% Output parameters:
-%  - corrImg: corrected image
-%   
-% 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function corrImg = correctImage(img, camInvResponse)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright 2006-2010 Jean-Francois Lalonde
-% Carnegie Mellon University
-% Do not distribute
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Corrects the image according to the inverse response function
+%
+%   corrImg = correctImage(img, camInvResponse)
+%
+%   'camInvResponse' is a Nx3 vector storing the inverse response function.
+%   If [], corrImg = img.
+%
+% ----------
+% Jean-Francois Lalonde
 
-% reshape and rescale in the [0,1] interval
-imgVec = reshape(img, size(img,1)*size(img,2), size(img,3));
-maxVal = max(imgVec, [], 1);
-imgVec = imgVec ./ repmat(maxVal, size(img,1)*size(img,2), 1);
-
-% clamp in [0,1] interval
-imgVec = min(max(imgVec, 0), 1);
-
-% correct
-corrImgVec = zeros(size(imgVec));
-for i=1:3
-     corrImgVec(:,i) = interp1(linspace(0, 1, length(camInvResponse(:,i))), camInvResponse(:,i), imgVec(:,i), 'linear');
+if isempty(camInvResponse)
+    corrImg = img;
+else
+    % reshape and rescale in the [0,1] interval
+    imgVec = reshape(img, size(img,1)*size(img,2), size(img,3));
+    maxVal = max(imgVec, [], 1);
+    imgVec = imgVec ./ repmat(maxVal, size(img,1)*size(img,2), 1);
+    
+    % clamp in [0,1] interval
+    imgVec = min(max(imgVec, 0), 1);
+    
+    % correct
+    corrImgVec = zeros(size(imgVec));
+    for i=1:3
+        corrImgVec(:,i) = interp1(linspace(0, 1, length(camInvResponse(:,i))), camInvResponse(:,i), imgVec(:,i), 'linear');
+    end
+    
+    % rescale and reshape back to original format
+    corrImgVec = corrImgVec .* repmat(maxVal, size(img,1)*size(img,2), 1);
+    corrImg = reshape(corrImgVec, size(img,1), size(img,2), size(img,3));
+    
 end
-
-% rescale and reshape back to original format
-corrImgVec = corrImgVec .* repmat(maxVal, size(img,1)*size(img,2), 1);
-corrImg = reshape(corrImgVec, size(img,1), size(img,2), size(img,3));
